d61ada3de22858d6f0a3df1386d34b0740dd5587
Richard <47975318+rtruon11@users.noreply.github.com>
2019-10-02 03:24:20 -0400

    Delete screapper test.py

--- screapper test.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
-quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
-page = Request(quote_page)
-soup = BeautifulSoup(page, 'html.parser')
-name_box = soup.find('h1', attrs={'class': 'name'})
-name = name_box.text.strip()
-print(name)
